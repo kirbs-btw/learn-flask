d239f8440edc50b60494d6bf3b1e516ebd73c04b
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2024-01-22 21:08:41 +0100

    missing comit

--- website/__init__.py
+++ website/__init__.py
@@ -7,9 +7,11 @@ def create_app():
     # telling app the sites exist 
     from .views import views
     from .auth import auth
+    from .api import api
     
     # register blueprints
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(api, url_prefix='/')
     
     return app
